99e3dc77d890f959f4ca56f426d4668f239e8ece
Lukas Cerny <luc.cerny@gmail.com>
2018-06-12 10:08:38 +0200

    checkout to HEAD

--- src/main/java/de/oth/ajp/jit/options/Checkout.java
+++ src/main/java/de/oth/ajp/jit/options/Checkout.java
@@ -2,14 +2,17 @@ package de.oth.ajp.jit.options;
 
 
 import de.oth.ajp.jit.core.Option;
+import de.oth.ajp.jit.domain.CommitFile;
 import de.oth.ajp.jit.util.Files;
 import de.oth.ajp.jit.util.JitFiles;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Optional;
 
 import static de.oth.ajp.jit.util.JitFiles.readCommits;
+import static de.oth.ajp.jit.util.JitFiles.readLastCommit;
 import static de.oth.ajp.jit.util.JitFiles.walk;
 import static de.oth.ajp.jit.util.Logger.print;
 
@@ -27,8 +30,14 @@ public class Checkout implements Option {
 
         try {
             walk().map(Path::toFile).forEach(File::delete);
-            readCommits().filter(c -> c.getHash().startsWith(hash))
-                    .findFirst().map(Files::readCommitTree)
+            Optional<CommitFile> optionalCommit;
+            if (hash.equals("HEAD")) {
+                optionalCommit = readLastCommit();
+            } else {
+                optionalCommit = readCommits().filter(c -> c.getHash().startsWith(hash)).findFirst();
+
+            }
+            optionalCommit.map(Files::readCommitTree)
                     .ifPresentOrElse(list -> list.forEach(JitFiles::copyCommit), this::commitNotFound);
         } catch (IOException e) {
             print(e.getMessage());
